13f2a05156b599059f582cf28587a6a2fe0b2e69
adi-yoshay <Yoshay87@gmail.com>
2026-01-01 16:52:24 +0200

    Add Python tests to CI workflow

---
+++ test_main.py
@@ -0,0 +1,14 @@
+import unittest
+from main import add
+
+
+class TestAddFunction(unittest.TestCase):
+
+    def test_add_two_numbers(self):
+        result = add(2, 3)
+        self.assertEqual(result, 5)
+
+
+if __name__ == "__main__":
+    unittest.main()
+
